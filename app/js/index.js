a2f7a7983edcdd8c85ff5d3a702b59215d11515f
Shane Herft <shaneherft@gmail.com>
2016-02-21 20:33:10 +1100

    Upload fix

---
+++ app/js/index.js
@@ -0,0 +1,173 @@
+var onAuthorize = function() {
+    updateLoggedIn();
+    $("#singleCard").hide();
+    $("#listOutput").empty();
+
+    Trello.members.get("me", function(member){
+        $("#fullName").text(member.fullName);
+
+        var $cards = $("<div>")
+            .text("Loading list...")
+            .appendTo("#listOutput");
+        // Output a list of all of the cards that the member
+        // is assigned to
+        Trello.get("lists/55d26f54726fb67f022db618/cards", function(cards) {
+            $cards.empty();
+            $.each(cards, function(ix, card) {
+                $("<a>")
+                // .attr({href: card.url, target: "trello"})
+                .addClass("card")
+                .text(card.name)
+                .appendTo($cards)
+                .click(function () {
+                  $('#listSelect').remove();
+                  cardSelected(card.id);
+                });
+            });
+        });
+    });
+
+};
+
+var cardSelected = function(selectedCard) {
+
+      $("#singleCard").show();
+
+      var $cards = $("<div>")
+          .empty()
+          .text("Loading card...")
+          .appendTo("#cardOutput");
+
+      var $labels = $("<div>")
+          .appendTo("#label");
+
+      Trello.get("cards/" + selectedCard, function(card) {
+          // window.resizeTo(266,58);
+
+          $cards.empty();
+              $("<a>")
+              // .attr({href: card.url, target: "trello"})
+              .addClass("cards")
+              .text(card.name)
+              .appendTo($cards);
+
+              var cardLink = card.url
+              console.log(cardLink);
+
+              $(".toggle").click(function(event) {
+              event.preventDefault();
+              $("div.overlay").fadeToggle("fast");
+
+              });
+
+              $(".toggle")
+              .mouseover(function() {
+                $('.icons').show();
+                // $('a.trello').attr('href','www.google.com');
+                  })
+              .mouseout(function() {
+                $('.icons').hide();
+                  });
+
+
+              //Displays correct color label
+              var cardLabel = card.labels[0].color;
+
+              switch (cardLabel) {
+                case "red":
+                  $(".cardLabel")
+                  .css("background-color", "#eb5a46");
+                  break;
+
+                case "sky":
+                  $(".cardLabel")
+                  .css("background-color", "#00c2e0");
+                  break;
+
+                case "green":
+                  $(".cardLabel")
+                  .css("background-color", "#61bd4f");
+                  break;
+
+                case "orange":
+                  $(".cardLabel")
+                  .css("background-color", "#ffab4a");
+                  break;
+
+                case "yellow":
+                  $(".cardLabel")
+                  .css("background-color", "#f2d600");
+                  break;
+
+                case "blue":
+                  $(".cardLabel")
+                  .css("background-color", "#0079bf");
+                  break;
+
+                case "pink":
+                  $(".cardLabel")
+                  .css("background-color", "#ff80ce");
+                  break;
+
+                case "lime":
+                  $(".cardLabel")
+                  .css("background-color", "#51e898");
+                  break;
+
+                case "purple":
+                  $(".cardLabel")
+                  .css("background-color", "#c377e0");
+                  break;
+
+                case "black":
+                  $(".cardLabel")
+                  .css("background-color", "#4d4d4d");
+                  break;
+
+                default:
+                  console.log(cardLabel + " does not work :(");
+              };
+
+              $(".trello")
+              .click(function(){
+                  window.open(cardLink,"Card Link","width=760,height=660,titlebar=yes,scrollbars=no,status=yes,frame=yes");
+                  });
+
+              $(".settings")
+              .click(function(){
+                  document.location.reload(true);
+                  });
+
+      });
+
+};
+
+
+//DO NOT TOUCH
+
+var updateLoggedIn = function() {
+    var isLoggedIn = Trello.authorized();
+    $("#loggedout").toggle(!isLoggedIn);
+    $("#loggedin").toggle(isLoggedIn);
+};
+
+var logout = function() {
+    Trello.deauthorize();
+    updateLoggedIn();
+};
+
+Trello.authorize({
+    interactive:false,
+    success: onAuthorize
+});
+
+$("#connectLink")
+.click(function(){
+    Trello.authorize({
+        type: "redirect",
+        success: onAuthorize,
+        name: "Toptask"
+    })
+});
+
+$("#disconnect").click(logout);
---
+++ main.js
@@ -0,0 +1,57 @@
+// Dependencies
+var electron = require('electron'),
+    app = electron.app,
+    BrowserWindow = electron.BrowserWindow;
+
+// const ipcMain = require('electron').ipcMain;
+
+// Config
+var mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+      width: 266,
+      height: 58,
+      frame: false,
+      alwaysOnTop: true,
+      webPreferences: {
+          "node-integration": false,
+      }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('http://localhost:8000');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
