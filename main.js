94318936b282085ba02ae5ec9da169e4bec25b63
mediaslav <info@mediaslav.com>
2016-02-29 18:55:35 +0300

    trello and ui fix

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
 // Dependencies
 var electron = require('electron'),
-    app = electron.app,
-    BrowserWindow = electron.BrowserWindow;
+  app = electron.app,
+  BrowserWindow = electron.BrowserWindow;
 
+var __DEV__ = (process.env['NODE_ENV'] === 'development');
 
+console.log(`NODE_ENV=[${process.env['NODE_ENV']}]`);
 
 // Config
 var mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
+function createWindow() {
+
+  if (__DEV__) {
+    // DEBUG run, developer helper features enabled
+
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      frame: 1,
+      alwaysOnTop: 0,
+      webPreferences: {"node-integration": false,}
+    });
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools({detach: true});
+
+  } else {
+
+    // PRODUCTION run
+
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
       width: 266,
       height: 58,
       frame: false,
       alwaysOnTop: true,
       webPreferences: {
-          "node-integration": false,
-          // "preload": "file:///Users/shaneherft/Google%20Drive/Development/Toptask/app/js/preload.js"
-          // "preload": path.join(__dirname, 'preload.js')
+        "node-integration": false,
+        // "preload": "file:///Users/shaneherft/Google%20Drive/Development/Toptask/app/js/preload.js"
+        // "preload": path.join(__dirname, 'preload.js')
       }
+    });
+  }
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
   });
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:8000');
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
-
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
