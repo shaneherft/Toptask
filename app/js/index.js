0b154679a9545a307f88c79845cdfd19f8248c73
Shane Herft <shaneherft@gmail.com>
2016-03-25 17:41:23 +1100

    Timer store

--- app/js/index.js
+++ app/js/index.js
@@ -313,6 +313,7 @@ jQuery(document).ready(function ($) {
       ipcRenderer.send('set-size', 269, 66);
       $('#welcome-loading').show();
       $('#listOutput').empty();
+      clearTimeout(t);
       getList();
     });
 
@@ -340,6 +341,7 @@ jQuery(document).ready(function ($) {
       event.stopPropagation();
     });
 
+    timer();
 
   ipcRenderer.on("refresh-card", function() {
     cardSelected(currentCard.id, cardNumber);
@@ -422,6 +424,49 @@ $("#zoom a").click(function(e) {
        e.preventDefault();
     }
 );
+
+var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "0") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00")  + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
+
+
+/* Start button */
+// start.onclick = timer;
+
+/* Stop button */
+// stop.onclick = function() {
+//     clearTimeout(t);
+// }
+
+/* Clear button */
+// clear.onclick = function() {
+//     h1.textContent = "00:00";
+//     seconds = 0; minutes = 0; hours = 0;
+// }
 
 //TRELLO AUTH
 
